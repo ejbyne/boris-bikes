a095fca2d4cef133da3c950747e615436859d8cd
ed <ejbyne@gmail.com>
2014-10-28 20:11:51 +0000

    Added capacity and maximum capacity to DockingStation class

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,8 +2,12 @@ require 'docking_station'
 
 describe DockingStation do
 
+  def fill_station(station)
+    20.times { station.dock(Bike.new) }
+  end
+
   let(:bike) { Bike.new }
-  let(:station) { DockingStation.new }
+  let(:station) { DockingStation.new(:capacity => 20) }
   
   it "should accept a bike" do
     expect(station.bike_count).to eq(0)
@@ -15,6 +19,17 @@ describe DockingStation do
     station.dock(bike)
     station.release(bike)
     expect(station.bike_count).to eq(0)
+  end
+
+  it "should know when it's full" do
+    expect(station).not_to be_full
+    fill_station(station)
+    expect(station).to be_full
+  end
+
+  it "should not accept a bike when it's full" do
+    fill_station(station)
+    expect(lambda {station.dock(bike) }).to raise_error(RuntimeError)
   end
 
 end
