98b5d63e0312f145343a0adc2d8a98de528d9751
ed <ejbyne@gmail.com>
2014-10-28 17:51:47 +0000

    Added first Bike test

---
+++ spec/bike_spec.rb
@@ -0,0 +1,11 @@
+require 'bike'
+
+describe Bike do
+
+  it "should not be broken after we create it" do
+    the_bike = Bike.new
+    expect(the_bike).not_to be_broken
+    #expect(the_bike.broken?).to be false
+  end
+
+end
